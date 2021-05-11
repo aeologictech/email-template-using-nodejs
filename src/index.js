14ca2ee70b609de4fa4685f8ed02be4f6758ce7d
Purti Aggarwal <purti@aeologic.com>
2021-05-11 18:46:52 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+const viewPath =  path.resolve(__dirname, './templates/views/');
+const partialsPath = path.resolve(__dirname, './templates/partials');
+
+app.use(express.static(path.join(__dirname, './public')))
+app.post('/send-mail', (req, res) => {
+  res.status(200).send({
+    status: "200",
+    message: 'Mail Sent!'
+  })
+  sendMail();
+})
+const sendMail = () => {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'Sender Email ID',
+      pass: 'Sender Email ID Password'
+    }
+  });
+  transporter.use('compile', hbs({
+    viewEngine: {
+      extName: '.handlebars',
+      // partialsDir: viewPath,
+      layoutsDir: viewPath,
+      defaultLayout: false,
+      partialsDir: partialsPath,
+      express
+    },
+    viewPath: viewPath,
+    extName: '.handlebars',
+  }))
+
+  var mailOptions = {
+    from: 'Sender Email ID',
+    to: 'Receiver Email Id',
+    subject: 'Sending Email using Node.js',
+    template: 'index',
+    attachments: [
+      { filename: 'abc.jpg', path: path.resolve(__dirname, './image/abc.jpg')}
+    ]
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+}
+
+app.listen(port , () => {
+    console.log(`server up to ${port}`)
+}) 
